f0516698bac4a2dd0f7d59948a32db845bae51e4
esmailireza <esmailireza20@gmail.com>
2022-02-04 21:41:45 +0330

    add blur to offer section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-let countDownDate = new Date("Feb 5, 2022 15:37:25").getTime();
+let countDownDate = new Date("Feb 4, 2022 15:37:25").getTime();
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
@@ -24,6 +24,8 @@ let x = setInterval(function () {
 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "به پایان رسید";
+    document.getElementById("demo").innerHTML = "fiwief";
+    document.getElementById("offer-expire-text").innerHTML = "به پایان رسید";
+    document.getElementById("offer-blur").style.filter = "blur(2px)";
   }
 }, 1000);
